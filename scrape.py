45e8eb2813cbdc66c5a3d6087c75f72be3db1abd
Jon Jablonski <jonjab@ucsb.edu>
2018-02-05 20:33:15 -0800

    First bit of code to open URL
    Our basic script for the day.  It only worked for Raleigh.

---
+++ scrape.py
@@ -0,0 +1,16 @@
+# small test to open a page at Dianpint
+#import libraries
+#this works for Raleigh
+import urllib
+from bs4 import BeautifulSoup
+from urllib.Request import Request, urlopen
+
+#Getting around thinking we're a robot
+site = 'http://dianping.com'
+hdr = {'User-Agent': 'Mozilla/5.0'}
+req = Request(site,headers=hdr)
+
+#open URL
+page = urlopen(req)
+soup = BeautifulSoup(page)
+print(soup)
